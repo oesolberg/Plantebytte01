2f5232a9180014193050a99f78bf4dfd21fb0493
Odd Erik Midthaug <oesolberg@hotmail.com>
2022-06-21 16:32:54 +0200

    favicon

--- PlanteBytte01/Program.cs
+++ PlanteBytte01/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using PlanteBytte01;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
